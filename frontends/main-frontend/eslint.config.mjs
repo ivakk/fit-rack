d9ba05fea93988a0514daef46e189d946f04cc58
Ivaylo Chevarganov <ivk@Ivaylos-MacBook-Air.local>
2026-06-21 13:35:34 +0200

    fixed ci issues

--- frontends/main-frontend/eslint.config.mjs
+++ frontends/main-frontend/eslint.config.mjs
@@ -9,5 +9,5 @@ export default defineConfig([
       "react-hooks/set-state-in-effect": "off",
     },
   },
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  globalIgnores([".next/**", "out/**", "build/**", "coverage/**", "next-env.d.ts"]),
 ]);
